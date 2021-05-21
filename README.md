6ee00641e575f87ab004f19dea6e5cfa3fb584fc
IanAragonS <a01177009@itesm.mx>
2021-05-20 19:25:34 -0500

    made big changes to everything, working on user funcs, admin is finished

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # proyecto_web
+
+To install the modules needed use this line in the command line:
+
+	npm install --save mongodb express cors ejs axios jsonwebtoken cookie-parser bcryptjs fs path
+	
+Use this to run it:
+	
+	node app.js
+	
+Or use this to run even when changes occur:
+
+	nodemon app.js
--- app.js
+++ app.js
@@ -3,44 +3,60 @@ const app = express();
 var cors = require('cors');
 const port = 3000;
 const axios = require('axios').default;
-app.use(cors());
-app.use(express.json()); // body-parser
 var MongoClient = require('mongodb').MongoClient;
 var mongo = require('mongodb');
 const ejs = require('ejs');
-
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bcrypt = require("bcryptjs");
+const fs = require('fs');
+const path = require('path');
+ 
 var collection;
+var cart;
+var users;
 
+app.use(cors());
+app.use(express.json()); // body-parser
+app.use(cookieParser());
 app.use(express.static('./'));
+
+const secret = "5tr0n6P@55W0rD";
 
 const uri = "mongodb+srv://wanderer:hahaesto123@cluster0.6qswt.mongodb.net/cartDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
 client.connect(err => {
 	collection = client.db("cartDB").collection("products"); 
+    cart = client.db("cartDB").collection("cart");
+    users = client.db("cartDB").collection("user");
 });
 
-//pages loading backend
-app.get('/cart', (req, res) => {
-    res.sendFile("./shopping-cart.html", {root: __dirname});    
+app.get('/login', function (req, res) {
+    res.sendFile('login.html', {root: './pages/'});
 });
 
-app.get('/add', (req, res) => {
-    res.sendFile("./add.html", {root: __dirname});    
+app.get('/register', function (req, res) {
+    res.sendFile('register.html', {root: './pages/'});
 });
 
-app.post('/edit', (req, res) => {
-	res.sendFile("./edit-product.html", {root: __dirname}); 
-});
-
-app.route('/product/:id/edit').get((req, res) => {
-    let productId  = req.params.id;
-    ejs.renderFile("./edit-product.html", {productId: productId}, null, function(err, str){
+app.get('/products', requireLogin, function (req, res) {
+    ejs.renderFile('./products.html', {user: req.user}, null, function(err, str){
         if (err) res.status(503).send(`error when rendering the view: ${err}`); 
         else {
             res.end(str);
         }
     });
 });
+
+app.get('/', requireLogin, function (req, res) {
+    ejs.renderFile('./pages/main.html', {user: req.user}, null, function(err, str){
+        if (err) res.status(503).send(`error when rendering the view: ${err}`); 
+        else {
+            res.end(str);
+        }
+    });
+});
+
 
 /*
 products/:id delete method: To remove a product from the database. --
@@ -50,13 +66,132 @@ users/:id get method: To get the data of a user
 users/:id put method: To modify the data of a user
 users/:id delete method: To delete a user
 
-/login
-/logout
 /completePurchase
 */
 
+
+app.post('/login', function (req, res) {
+    const { email, password } = req.body;
+    const user = users.findOne(); 
+    console.log(user);
+    if(user.password != password){res.status(403).send('Invalid credentials'); return; }
+    
+    if (user) {
+        console.log(`Succesfully logged ${email} in`);
+        // Generate an access token
+        const accessToken = generateToken(user);
+        res.cookie("authorization", accessToken, {secure: true, httpOnly: true});
+        res.status(200).json(accessToken);
+    }else{
+        res.status(403).send('Invalid credentials');
+    }
+});
+
+app.post('/logout', requireLogin, function(req, res){
+    console.log(`Logging out ${req.username}`)
+    res.clearCookie('authorization');
+    res.send('User logged out');
+});
+
+
+
+//users backend ----------------------------------------------------------------------------------------------
+function generateToken(user) {
+    let payload = {
+     username: user.username,
+     id: user.id,
+     role: user.role
+    };
+    let oneDay = 60 * 60 * 24;
+    return token = jwt.sign(payload, secret, { expiresIn: oneDay });
+}
+
+// middleware that add the user
+function requireLogin(req, res, next){
+    let accessToken = req.cookies.authorization
+    // if there is no token stored in cookies, the request is unauthorized
+    if (!accessToken){ 
+        console.log('Unauthorized user, redirecting to login'); 
+        return res.redirect('/login'); 
+    }
+
+    try{
+        // use the jwt.verify method to verify the access token, itthrows an error if the token has expired or has a invalid signature
+        payload = jwt.verify(accessToken, secret)
+        console.log('Logged user accessing the site ' + payload.username);
+        req.user = payload; // you can retrieve further details from the database. Here I am just taking the name to render it wherever it is needed.
+        next()
+    }
+    catch(e){
+        //if an error occured return request unauthorized error, or redirect to login
+        // return res.status(401).send():
+        res.redirect(403, '/login');
+    }
+}
+
+// middleware that add the user
+function requireAdminLogin(req, res, next){
+    let accessToken = req.cookies.authorization
+    // if there is no token stored in cookies, the request is unauthorized
+    if (!accessToken){ 
+        console.log('Unauthorized user, redirecting to login'); 
+        return res.redirect('/login'); 
+    }
+
+    try{
+        // use the jwt.verify method to verify the access token, itthrows an error if the token has expired or has a invalid signature
+        payload = jwt.verify(accessToken, secret)
+		if(payload.role != 'admin'){
+			console.log('Unauthorized user, redirecting to login');
+			return res.redirect('/products'); 
+		}
+        console.log('Logged user accessing the site ' + payload.username);
+        req.user = payload; // you can retrieve further details from the database. Here I am just taking the name to render it wherever it is needed.
+        next()
+    }
+    catch(e){
+        //if an error occured return request unauthorized error, or redirect to login
+        // return res.status(401).send():
+        res.redirect(403, '/login');
+    }
+}
+
+
+
+//
+// Admin functions -----------------------------------------------------------------------------------------------
+//
+
+
+//pages loading backend
+app.get('/root/admin/products', requireAdminLogin, function(req, res) {
+    res.sendFile("./pages/admin-products.html", {root: __dirname});    
+});
+
+app.get('/root/admin/add', requireAdminLogin, function(req, res) {
+    res.sendFile("./pages/add.html", {root: __dirname});    
+});
+
+app.post('/root/admin/edit', requireAdminLogin, function(req, res) {
+	res.sendFile("./pages/edit-product.html", {root: __dirname}); 
+});
+
+app.get('/root/admin/product/:id/edit', requireAdminLogin, function(req, res) {
+    let productId  = req.params.id;
+    ejs.renderFile("./pages/edit-product.html", {productId: productId}, null, function(err, str){
+        if (err) res.status(503).send(`error when rendering the view: ${err}`); 
+        else {
+            res.end(str);
+        }
+    });
+});
+
+
+
+
+
 //products backend
-app.route('/product/:id').put((req, res) => {
+app.put('/root/admin/product/:id', requireAdminLogin, function(req, res) {
     let productId  = req.params.id;
     let{ name, price, brand} = req.body;
     var id = new mongo.ObjectID(productId);
@@ -66,7 +201,7 @@ app.route('/product/:id').put((req, res) => {
 	});
 })
 
-app.route('/product/:id').get(async function(req, res) {
+app.get('/root/admin/product/:id', requireAdminLogin, async function(req, res) {
     let productId  = req.params.id;
     let product = await collection.find().toArray();
 	product.forEach(pro => {
@@ -76,12 +211,12 @@ app.route('/product/:id').get(async function(req, res) {
 	});	
 })
 
-app.route('/products').get(async function(req, res){  
+app.get('/root/admin/products', requireAdminLogin, async function(req, res){  
     let products = await collection.find().toArray(); 
     res.send(products);
 });
 
-app.post('/removeProduct', function (req, res) {
+app.post('/root/admin/removeProduct', requireAdminLogin, function(req, res) {
 
     let productId  = req.query.id;
 	collection.deleteOne({
@@ -92,8 +227,7 @@ app.post('/removeProduct', function (req, res) {
 	});
 })
 
-
-app.post('/product/create', function (req, res) {
+app.post('/root/admin/product/create', requireAdminLogin, function(req, res) {
 
     let nameParam = req.query.name;
 	let priceParam = req.query.price;
@@ -107,11 +241,10 @@ app.post('/product/create', function (req, res) {
 		if (err) throw err;
 		console.log("1 document inserted");
 	});
+})
 
- })
 
 
-//users backend
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
